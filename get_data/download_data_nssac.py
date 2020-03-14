bb69a6fcc32f904c2b23f47ed9d846baa12a5bb9
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-03-14 14:17:21 +0100

    Downloading summary data from NSSAC Uni. of Viriginia.

---
+++ get_data/download_data_nssac.py
@@ -0,0 +1,38 @@
+"""
+Downloading historical data from Network Systems Science and Advanced Computing (NSSAC)
+of the University of Virginia, dashboard API.
+The dasboard can be found here: http://nssac.bii.virginia.edu/covid-19/dashboard/.
+The CSV files contain on Confirmed cases, Deaths and Recovered cases from over 100
+countries (at the time of writing).
+"""
+from datetime import datetime, timedelta
+import time
+import requests
+import get_data.utils as utils
+
+base_url = "http://nssac.bii.virginia.edu/covid-19/dashboard/data/nssac-ncov-sd-{date}.csv"
+
+DATA_FOLDER = '../data/historical'
+DATE_FORMAT = "%m-%d-%Y"
+SLEEP_SEC = 3
+start_date = datetime.strptime("01-23-2020", DATE_FORMAT)
+end_date = datetime.now()
+
+# Downloading summary data
+csv_url = base_url.format(date="summary")
+response = requests.get(csv_url)
+response.raise_for_status()
+utils.save(response, DATA_FOLDER)
+
+
+# Downloading historical CSV's
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
+# for date in daterange(start_date, end_date + timedelta(1)):
+#     date_string = date.strftime(DATE_FORMAT)
+#     csv_url = base_url.format(date=date_string)
+#     print("Downaloading data from:", csv_url)
+#     time.sleep(SLEEP_SEC)
