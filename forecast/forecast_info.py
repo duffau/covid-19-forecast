55e248241bbf64c473ce865fdfbdbafe60793def
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-03-19 18:26:58 +0100

    Added id to forecast info.

--- forecast/forecast_info.py
+++ forecast/forecast_info.py
@@ -4,7 +4,8 @@ from pandas import DataFrame
 
 class ForecastInfo:
 
-    def __init__(self, forecast_time: datetime, latest_data_point: datetime):
+    def __init__(self, id: str, forecast_time: datetime, latest_data_point: datetime):
+        self.id = id
         self.forecast_time = forecast_time
         self.latest_data_point = latest_data_point
 
@@ -12,8 +13,9 @@ class ForecastInfo:
         return f'forecast_time={self.forecast_time}\nlatest_data_point={self.latest_data_point}'
 
 
-def extract_cssegi_forecast_info(df: DataFrame):
+def extract_cssegi_forecast_info(country: str, df: DataFrame):
     return ForecastInfo(
+        id=country,
         forecast_time=datetime.now(),
         latest_data_point=df.date.max().to_pydatetime()
     )
--- tests/forecast/forecast_info_test.py
+++ tests/forecast/forecast_info_test.py
@@ -5,6 +5,7 @@ from pandas import DataFrame
 
 def test_forecast_info_init():
     ForecastInfo(
+        id='some string',
         forecast_time=datetime(2020, 3, 19, 17, 59),
         latest_data_point=datetime(2020, 3, 18, 12, 0)
     )
@@ -13,6 +14,6 @@ def test_forecast_info_init():
 def test_extract_cssegi_forecast_info_datetime():
     max_date = datetime(2020, 3, 18, 12, 00)
     df = DataFrame({'date': [datetime(2020, 3, 15, 18, 00), max_date]})
-    fc_info = extract_cssegi_forecast_info(df)
+    fc_info = extract_cssegi_forecast_info('Denmark', df)
     assert fc_info.latest_data_point == max_date
     assert isinstance(fc_info.forecast_time, datetime)
