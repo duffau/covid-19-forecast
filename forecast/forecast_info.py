c843ade783e776704280d56ea763d25a410644c0
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-03-20 10:08:08 +0100

    Made to_ini config file format method in forecast info class.

--- forecast/forecast_info.py
+++ forecast/forecast_info.py
@@ -4,18 +4,37 @@ from pandas import DataFrame
 
 class ForecastInfo:
 
-    def __init__(self, id: str, forecast_time: datetime, latest_data_point: datetime):
+    def __init__(self, id: str, model_name: str, forecast_time: datetime, latest_data_point: datetime):
         self.id = id
+        self.model_name = model_name
         self.forecast_time = forecast_time
         self.latest_data_point = latest_data_point
 
     def __str__(self):
-        return f'forecast_time={self.forecast_time}\nlatest_data_point={self.latest_data_point}'
+        sorted_attributes = sorted(self.__dict__.items(), key=lambda item: item[0])
+        public_attributes = [(k, v) for k, v in sorted_attributes if not k.startswith('_')]
+        return '\n'.join([f'{k} = "{v}"' for k, v in public_attributes])
+
+    def to_ini(self):
+        header = "[forecast-info]"
+        body = str(self)
+        return "\n".join([header, body])
 
 
-def extract_cssegi_forecast_info(country: str, df: DataFrame):
+def extract_cssegi_forecast_info(country: str, model_name: str, df: DataFrame):
     return ForecastInfo(
         id=country,
+        model_name=model_name,
         forecast_time=datetime.now(),
         latest_data_point=df.date.max().to_pydatetime()
     )
+
+
+if __name__ == '__main__':
+    fi = ForecastInfo(
+        id='some string',
+        model_name='SIR',
+        forecast_time=datetime(2020, 3, 19, 17, 59),
+        latest_data_point=datetime(2020, 3, 18, 12, 0)
+    )
+    print(str(fi))
--- tests/forecast/forecast_info_test.py
+++ tests/forecast/forecast_info_test.py
@@ -1,3 +1,4 @@
+import pytest
 from forecast.forecast_info import ForecastInfo, extract_cssegi_forecast_info
 from datetime import datetime
 from pandas import DataFrame
@@ -6,14 +7,36 @@ from pandas import DataFrame
 def test_forecast_info_init():
     ForecastInfo(
         id='some string',
+        model_name='SIR',
         forecast_time=datetime(2020, 3, 19, 17, 59),
         latest_data_point=datetime(2020, 3, 18, 12, 0)
     )
 
 
+@pytest.fixture
+def fc_info():
+    return ForecastInfo(
+        id='some string',
+        model_name='SIR',
+        forecast_time=datetime(2020, 3, 19, 17, 59),
+        latest_data_point=datetime(2020, 3, 18, 12, 0)
+    )
+
+
+def test_forecast_info_str(fc_info):
+    assert str(fc_info)
+
+
+def test_forecast_info_ini_format(fc_info):
+    config_str = fc_info.to_ini()
+    lines = config_str.split('\n')
+    n_attr = 4
+    assert len(lines) == n_attr + 1
+
+
 def test_extract_cssegi_forecast_info_datetime():
     max_date = datetime(2020, 3, 18, 12, 00)
     df = DataFrame({'date': [datetime(2020, 3, 15, 18, 00), max_date]})
-    fc_info = extract_cssegi_forecast_info('Denmark', df)
+    fc_info = extract_cssegi_forecast_info('Denmark', 'The Model', df)
     assert fc_info.latest_data_point == max_date
     assert isinstance(fc_info.forecast_time, datetime)
