943b57852dca2be27b7843d2647057fe49d475db
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-03-22 20:28:18 +0100

    Fitting SIR in analysis based on unit population.

--- analysis/fit_sir_model.py
+++ analysis/fit_sir_model.py
@@ -11,7 +11,7 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 1000)
 PLOT_FOLDER = 'plots/forecasts'
 DATA_FILE = '../data/pre-processed/cssegi_sand_data/cssegi_agg_data.pickle'
-RANDOM_SEED = 43
+RANDOM_SEED = 44
 SCALE_FACTOR = 1000
 INFECTIOUS_PERIOD = 2.9
 GAMMA = 1/INFECTIOUS_PERIOD
@@ -20,11 +20,14 @@ model_name = 'SIR'
 Params = namedtuple('Params', ['beta', 'R0', 'I0'])
 
 t0 = 0
-n_days_predict = 100
+n_days_predict = 5
 # N = 120000
 start_params = {
-    # 'China': Params(beta=0.06138, gamma=0.046619, I0=2.02930572e+04, S0=1000000),
-    'Denmark': Params(beta=2.*GAMMA, R0=100., I0=10.)
+    'Denmark': Params(beta=0.6, R0=None, I0=None),
+    'Iran': Params(beta=0.6, R0=None, I0=None),
+    'Italy': Params(beta=0.6, R0=None, I0=None),
+    'Spain': Params(beta=0.6, R0=None, I0=None),
+    'Sweden': Params(beta=0.6, R0=None, I0=None),
 }
 
 countries = ['Denmark', 'Iran', 'Sweden', 'Italy', 'Spain']
@@ -43,11 +46,12 @@ for country in countries:
     except AttributeError:
         dates_obs = pd.to_datetime(df.last_update_date).values
 
-    susceptible_obs = df.total_susceptible.values/SCALE_FACTOR
-    infected_obs = df.total_infected.values/SCALE_FACTOR
-    removed_obs = df.total_removed.values/SCALE_FACTOR
+    population = df.population.iloc[0]
+    susceptible_obs = df.total_susceptible.values/population
+    infected_obs = df.total_infected.values/population
+    removed_obs = df.total_removed.values/population
     y_obs = susceptible_obs, infected_obs, removed_obs
-    N = df.population.iloc[0]/SCALE_FACTOR
+    N = 1.
     print(f'N: {N}')
 
     def sir_deriv(t, y, N, beta, gamma):
@@ -60,7 +64,7 @@ for country in countries:
 
     def eval_sir_model(beta, gamma, I0, S0, R0, N, t_span, t_eval=None):
         y0 = (S0, I0, R0)
-        print(f'beta = {beta}, gamma = {gamma}, R0 = {R0}, S0 = {S0}, I0 = {I0}')
+        # print(f'beta = {beta}, gamma = {gamma}, R0 = {R0}, S0 = {S0}, I0 = {I0}')
         sol = solve_ivp(sir_deriv, t_span, y0, t_eval=t_eval, args=(N, beta, gamma))
         return sol.y
 
@@ -72,7 +76,7 @@ for country in countries:
         val += ((S_hat_t - S_t)**2).mean()
         val += ((I_hat_t - I_t)**2).mean()
         val += ((R_hat_t - R_t)**2).mean()
-        print('sumsq:', val)
+        # print('sumsq:', val)
         return val
 
     def minimize_wrapper(params):
@@ -86,21 +90,20 @@ for country in countries:
         return np.log(params)
 
 
-    if country in start_params:
-        beta, R0, I0 = start_params[country]
-    else:
-        np.random.seed(RANDOM_SEED)
-        random.seed(RANDOM_SEED)
-        beta = random.uniform(a=0.01, b=5.0)
-        # gamma = random.uniform(a=0.01, b=5.0)
-        R0 = removed_obs[0] if infected_obs[0] > 0 else random.uniform(a=1.0, b=500.0)/SCALE_FACTOR
-        I0 = infected_obs[0] if infected_obs[0] > 0 else random.uniform(a=1.0, b=500.0)/SCALE_FACTOR
-        S0 = random.uniform(a=susceptible_obs[0]*0.1, b=susceptible_obs[0])
+    beta, R0, I0 = start_params.get(country, Params(None, None, None))
+
+    np.random.seed(RANDOM_SEED)
+    random.seed(RANDOM_SEED)
+    if not beta:
+        beta = random.uniform(a=0.001, b=5.0)
+    if not R0:
+        R0 = removed_obs[0] if infected_obs[0] > 0 else random.uniform(a=0.001, b=1)/SCALE_FACTOR
+    if not I0:
+        I0 = infected_obs[0] if infected_obs[0] > 0 else random.uniform(a=0.001, b=1)/SCALE_FACTOR
 
     fit_params = repam([beta, R0, I0])
     gamma = GAMMA
     S0 = susceptible_obs[0]
-
 
     dates_pred = np.arange(dates_obs[-1] + np.timedelta64(1, 'D'), dates_obs[-1] + np.timedelta64(n_days_predict+1, 'D'), np.timedelta64(1, 'D'))
     dates_eval_pred = np.append(dates_obs, dates_pred)
@@ -110,8 +113,6 @@ for country in countries:
     t_eval_pred = (dates_eval_pred - dates_eval_pred[0]).astype('timedelta64[s]').astype('float64')/SECS_PER_DAY
     t_span = (min(t0, t_eval_pred.min()), t_eval_pred.max())
 
-    print("t_eval:", t_eval)
-    print("t_eval_pred:", t_eval_pred)
     res = minimize(
         minimize_wrapper, np.array(fit_params),
         method='Nelder-Mead',
@@ -120,16 +121,16 @@ for country in countries:
     print(res)
     beta, R0, I0 = inv_repam(res.x)
     print(f'R0 = {beta/gamma:.2f}')
+    print(f'beta = {beta}, gamma = {gamma}, R0 = {R0}, S0 = {S0}, I0 = {I0}')
     S_t, I_t, R_t = eval_sir_model(beta, gamma, I0, S0,  R0, N, t_span, t_eval_pred)
-
 
     fig = plt.figure(figsize=(8, 5))
     # plt.plot(dates_eval_pred, S_t, label="Susceptible")
-    plt.plot(dates_eval_pred, I_t, label=f"Infected ({SCALE_FACTOR} pers.)")
-    plt.plot(dates_eval_pred, R_t, label=f"Removed (Dead + Immune) ({SCALE_FACTOR} pers.)")
+    plt.plot(dates_eval_pred, I_t*population, label=f"Infected")
+    plt.plot(dates_eval_pred, R_t*population, label=f"Removed (Dead + Immune)")
     # plt.plot(dates_obs, susceptible_obs, "k*:", label="Susceptible - Observed ({SCALE_FACTOR} pers.)")
-    plt.plot(dates_obs, infected_obs, "k*:", label=f"Infected - Observed ({SCALE_FACTOR} pers.)")
-    plt.plot(dates_obs, removed_obs, "k*:", label=f"Removed - Observed ({SCALE_FACTOR} pers.)")
+    plt.plot(dates_obs, infected_obs*population, "k*:", label=f"Infected - Observed")
+    plt.plot(dates_obs, removed_obs*population, "k*:", label=f"Removed - Observed")
     plt.xticks(rotation=45)
     plt.grid("True")
     plt.title(country)
