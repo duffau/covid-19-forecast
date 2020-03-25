8f8b88512da00491cc88d46361cc7b6d103b6a22
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-03-25 17:22:09 +0100

    Added deploy script.

--- admin/deploy.py
+++ admin/deploy.py
@@ -1,10 +1,12 @@
 from subprocess import call
 from datetime import datetime
 
-date_str = datetime.now().strftime('%Y-%m-%d')
-commit_tag = 'v' + date_str
-commit_msg = f'Deployed forecast {date_str}'
 
-call(f'git commit -am "{commit_msg}"', shell=True)
-call(f'git tag {commit_tag}', shell=True)
-call('git push origin master', shell=True)
+def deploy():
+    date_str = datetime.now().strftime('%Y-%m-%d')
+    commit_tag = 'v' + date_str
+    commit_msg = f'Deployed forecast {date_str}'
+
+    call(f'git commit -am "{commit_msg}"', shell=True)
+    call(f'git tag {commit_tag}', shell=True)
+    call('git push origin master --tags', shell=True)
