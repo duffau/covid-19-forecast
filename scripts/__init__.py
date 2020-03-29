60e9266c1b2fce1e2b0069f43337f1239b96d297
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-03-29 15:14:43 +0200

    Default logging config in scripts.

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,2 +1,5 @@
 import logging.config
-logging.config.fileConfig(fname='logging.ini', disable_existing_loggers=False)
+try:
+    logging.config.fileConfig(fname='logging.ini', disable_existing_loggers=False)
+except KeyError:
+    logging.basicConfig(level=logging.INFO)
