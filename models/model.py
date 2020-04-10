02d23959b4cbfa2dd1bab14fcee6151f02a57341
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-04-10 19:18:33 +0200

    Added N to model simulate.

--- models/model.py
+++ models/model.py
@@ -14,5 +14,5 @@ class Model:
     def fit(self, y_obs, t_eval):
         raise NotImplementedError
 
-    def simulate(self, t_eval: Iterable[float]):
+    def simulate(self, t_eval: Iterable[float], N=1.0):
         raise NotImplementedError
--- models/seir.py
+++ models/seir.py
@@ -90,8 +90,8 @@ class SEIR(Model):
         dRdt = gamma * I
         return dSdt, dEdt, dIdt, dRdt
 
-    def simulate(self, t_eval: Iterable[float]):
+    def simulate(self, t_eval: Iterable[float], N=1.0):
         y0 = (self.params.S0, self.params.E0, self.params.I0, self.params.R0)
         t_span = (min(t_eval), max(t_eval))
         sol = solve_ivp(self._deriv, t_span, y0, t_eval=t_eval, args=(self.params.beta, self.params.gamma, self.params.alpha))
-        return sol.y
+        return sol.y * N
