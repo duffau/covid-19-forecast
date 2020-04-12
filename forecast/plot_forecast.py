b090ca39427b3d31990cd1e8d536d23fdfe4e203
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-04-12 14:30:27 +0200

    plotting beta time series instead of two different time horizons.

--- forecast/plot_forecast.py
+++ forecast/plot_forecast.py
@@ -1,25 +1,23 @@
-from typing import Iterable
+from typing import Iterable, Sequence
 from datetime import datetime
 from pandas import DataFrame, Series
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
 from forecast import ForecastInfo
-from models import SIRParams
-
-SCALING_FACTOR = 1000
 
 
 def plot_forecast(df_forecast: DataFrame, forecast_info: ForecastInfo, days_short_term=5, days_long_term=30) -> plt.Figure:
     fig = plt.figure(figsize=(8, 5), constrained_layout=True)
     gs = fig.add_gridspec(4, 3)
     long_term_plot = fig.add_subplot(gs[0:2, 0:2])
-    short_term_plot = fig.add_subplot(gs[2:4, 0:2])
+    beta_plot = fig.add_subplot(gs[2:4, 0:2])
     stat_box_1 = fig.add_subplot(gs[0, 2])
     stat_box_2 = fig.add_subplot(gs[1, 2])
     stat_box_3 = fig.add_subplot(gs[2, 2])
+    stat_box_4 = fig.add_subplot(gs[3, 2])
 
-    stats = calc_plot_stats(df_forecast, forecast_info.params)
+    stats = calc_plot_stats(df_forecast, forecast_info)
     r0 = stats['r0']
     peak_infected = stats['peak_infected']
     peak_date = stats['peak_date']
@@ -28,7 +26,8 @@ def plot_forecast(df_forecast: DataFrame, forecast_info: ForecastInfo, days_shor
 
     make_centered_text_box(stat_box_1, f'Peak date:\n {peak_date.strftime("%d-%m-%Y")}')
     make_centered_text_box(stat_box_2, f'Peak number of infected:\n {format_counts(peak_infected)}')
-    make_centered_text_box(stat_box_3, f'Basic reproducion number\n$R_0 = {r0:.2g}$')
+    # make_centered_text_box(stat_box_3, f'Basic reproducion number\n$R_0 = {r0:.2g}$')
+    # make_centered_text_box(stat_box_4, f'Current reproducion number\n$R_t = {rt:.2g}$')
 
     today = np.datetime64('now')
     short_term_mask = df_forecast.dates < today + np.timedelta64(days_short_term, 'D')
@@ -38,35 +37,38 @@ def plot_forecast(df_forecast: DataFrame, forecast_info: ForecastInfo, days_shor
         long_term_plot,
         dates=df_forecast.dates[long_term_mask],
         infected_forecast=df_forecast.infected_forecast[long_term_mask],
+        infected_obs=df_forecast.infected_obs[long_term_mask],
         title=f'Forecasting {days_long_term} days ahead.'
     )
 
-    make_infected_removed_plot(
-        short_term_plot,
-        dates=df_forecast.dates[short_term_mask],
-        infected_forecast=df_forecast.infected_forecast[short_term_mask],
-        infected_obs=df_forecast.infected_obs[short_term_mask],
-        removed_forecast=df_forecast.removed_forecast[short_term_mask],
-        removed_obs=df_forecast.removed_obs[short_term_mask],
-        title=f'Forecasting {days_short_term} days ahead.'
+    make_beta_plot(
+        beta_plot,
+        dates=Series(forecast_info.beta_dates),
+        beta_t=Series(forecast_info.beta_t),
+        title=f'Estimated transmission rate - Transmissions per infected per day.'
     )
 
     return fig
 
 
 def make_infected_removed_plot(ax, dates: Iterable[datetime],
-                               infected_forecast=Series(dtype='float64'), removed_forecast=Series(dtype='float64'), infected_obs=Series(dtype='float64'), removed_obs=Series(dtype='float64'),
-                               title='', ylabel="Persons (thousands)", date_formatter=DateFormatter("%d-%b")):
-    ax.set_title(title)
+                               infected_forecast: Sequence[float] = None,
+                               removed_forecast: Sequence[float] = None,
+                               infected_obs: Sequence[float] = None,
+                               removed_obs: Sequence[float] = None,
+                               title='', title_size=10,
+                               ylabel="Persons",
+                               date_formatter=DateFormatter("%d-%b")):
+    ax.set_title(title, fontdict={'fontsize': title_size})
 
-    if not infected_forecast.empty:
-        ax.plot(dates, infected_forecast, label="Infected")
-    if not removed_forecast.empty:
+    if infected_forecast is not None:
+        ax.plot(dates, infected_forecast, label="Infected - forecast")
+    if removed_forecast is not None:
         ax.plot(dates, removed_forecast, label="Removed (Recovered + Dead)")
-    if not infected_obs.empty:
-        ax.plot(dates, infected_obs, "k*:", label="Infected - Observed")
-    if not removed_obs.empty:
-        ax.plot(dates, removed_obs, "k*:", label="Removed - Observed")
+    if infected_obs is not None:
+        ax.plot(dates, infected_obs, "k.:", label="Infected - Observed")
+    if removed_obs is not None:
+        ax.plot(dates, removed_obs, "k.:", label="Removed - Observed")
 
     ax.xaxis.set_major_formatter(date_formatter)
     ax.tick_params(axis='x', rotation=25)
@@ -76,18 +78,39 @@ def make_infected_removed_plot(ax, dates: Iterable[datetime],
     return ax
 
 
-def make_centered_text_box(ax, textstr, size=12):
+def make_beta_plot(ax, dates: Sequence[datetime],
+                   beta_t: Sequence[float],
+                   title='', title_size=10,
+                   ylabel="Persons per day",
+                   date_formatter=DateFormatter("%d-%b")):
+    ax.set_title(title, fontdict={'fontsize': title_size})
+
+    ax.plot(dates, beta_t, label="Avg. transmission rate (beta)")
+    ax.xaxis.set_major_formatter(date_formatter)
+    ax.tick_params(axis='x', rotation=25)
+    ax.grid("True")
+    ax.set_ylabel(ylabel)
+    ax.legend()
+    return ax
+
+
+def make_centered_text_box(ax, textstr, size=11):
     ax.text(0.5, 0.5, textstr, size=size, ha="center", va="center")
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
     return ax
 
 
-def calc_plot_stats(df_forecast: DataFrame, params: SIRParams) -> dict:
+def calc_plot_stats(df_forecast: DataFrame, forecast_info: ForecastInfo) -> dict:
     peak_infected_index = df_forecast.infected_forecast.idxmax()
+    try:
+        r0 = forecast_info.params.beta / forecast_info.params.gamma
+    except AttributeError:
+        r0 = forecast_info.beta_t / forecast_info.params.gamma
+
     return {
-        'r0': params.beta / params.gamma,
-        'peak_infected': df_forecast.infected_forecast[peak_infected_index] * SCALING_FACTOR,
+        'r0': r0,
+        'peak_infected': df_forecast.infected_forecast[peak_infected_index],
         'peak_date': df_forecast.dates[peak_infected_index]
     }
 
