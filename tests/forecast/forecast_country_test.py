be4a9c84d231fa2c79026e89bdba21edcbede16e
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-04-12 14:29:02 +0200

    Testing forecast_country on two model classes (SIR and SIRt).

--- tests/forecast/forecast_country_test.py
+++ tests/forecast/forecast_country_test.py
@@ -1,9 +1,11 @@
 import pytest
 
 from models import SIRParams, SIR
-from forecast.forecast_country import forecast
+from models import SIRtParams, SIRt
+from forecast.forecast_country import forecast, ForecastInfo
 from forecast import extract_cssegi_forecast_info
 import pandas as pd
+import numpy as np
 from datetime import datetime
 
 COUNTRY = 'Country'
@@ -14,8 +16,12 @@ GAMMA = 0.35
 I0 = 0.001
 R0 = 0.0
 S0 = N - I0 - R0
+N_T_EVAL = 50
+N_PREDICT = 10
+T = range(30)
+BETA_T = np.interp(T, [0, 10], [0.65, 0.5])
 
-@pytest.fixture
+
 def sir_model():
     params = SIRParams(
         beta=BETA,
@@ -26,27 +32,49 @@ def sir_model():
     )
     return SIR(params=params)
 
-@pytest.fixture
-def df(sir_model):
-    n = 50
-    S, I, R = sir_model.simulate(t_eval=range(n), N=N_POP)
+
+
+def sirt_model():
+    params = SIRtParams(
+        gamma=GAMMA,
+        S0=S0,
+        I0=I0,
+        R0=R0
+    )
+    return SIRt(params=params, beta_t=BETA_T, t=T)
+
+
+def df_sim(model):
+    n = N_T_EVAL
+    S, I, R = model.simulate(t_eval=range(n), N=N_POP)
     return pd.DataFrame({
         'date': pd.date_range(datetime.today(), periods=n),
-        'country': [COUNTRY]*n,
-        'population': [N_POP]*n,
+        'country': [COUNTRY] * n,
+        'population': [N_POP] * n,
         'total_susceptible': S,
         'total_infected': I,
         'total_removed': R
     })
 
 
-def test_forecast(df, sir_model, mocker):
-    sir_model.fit = mocker.Mock(spec_set=sir_model.fit)
+def df_sir():
+    return df_sim(sir_model())
+
+
+def df_sirt():
+    return df_sim(sirt_model())
+
+
+@pytest.mark.parametrize("df,model", [(df_sir(), sir_model()), (df_sirt(), sirt_model())])
+def test_forecast(df, model, mocker):
+    model.fit = mocker.Mock(spec_set=model.fit)
     df_forecast, forecast_info = forecast(
         df=df,
         country=COUNTRY,
-        model=sir_model,
+        model=model,
         extract_forecast_info=extract_cssegi_forecast_info,
-        n_days_predict=10
+        n_days_predict=N_PREDICT
     )
-
+    assert isinstance(df_forecast, pd.DataFrame)
+    assert df_forecast.shape[0] == N_T_EVAL + N_PREDICT
+    assert isinstance(forecast_info, ForecastInfo)
