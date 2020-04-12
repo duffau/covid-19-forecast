8dd97706f9757b38961345e648471f30f0731643
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-04-12 19:48:21 +0200

    Updated make README with link to analysis changelog.

--- scripts/make_readme.py
+++ scripts/make_readme.py
@@ -20,6 +20,8 @@ README_TEMPLATE = '''
 
 Attempt to forecast the number of cases of COVID-19 around the world using the simple [SIR model][sir_model_wiki].
 
+The development of the estimation method is documented in the [CHANGELOG of the analysis section](analysis/CHANGELOG.md).
+
 ## Forecasts
 *Updated: {date}*
 
