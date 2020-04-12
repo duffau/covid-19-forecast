804b28ecbd643b4d18f8934e87d830ad6c8a36bc
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-04-12 14:30:47 +0200

    Forecasting using SIRt model instead of SIR model.

--- scripts/make_new_forecast.py
+++ scripts/make_new_forecast.py
@@ -3,14 +3,16 @@ import pandas as pd
 import os.path as op
 import pickle
 from datetime import datetime
-from models import SIRParams as Params
+from models import SIRtParams as Params
+from models import SIRt as Model
 from forecast import forecast_countries, extract_cssegi_forecast_info
 import scripts.config as config
 
 logger = logging.getLogger(__name__)
 
 N_DAYS_PREDICT = 400
-COUNTRIES = ['Denmark', 'Spain', 'Italy', 'Sweden']
+COUNTRIES = ['Denmark', 'Iran',  'Spain', 'Italy', 'Sweden']
+COUNTRIES = ['Denmark']
 SEED = 42
 
 
@@ -24,12 +26,13 @@ def main():
     df_all = pd.read_pickle(data_file)
 
     start_params = {
-        'Denmark': Params(beta=1.4863183028145124, gamma=None, R0=0.1, I0=1e-7, S0=None),
+        # 'Denmark': Params(gamma=None, R0=1e-9, I0=1e-5, S0=None),
     }
 
     df_forecasts, forecast_info_collection = forecast_countries(
         df_all,
         col_country='country',
+        model_class=Model,
         extract_forecast_info=extract_cssegi_forecast_info,
         n_days_predict=N_DAYS_PREDICT,
         start_params_collection=start_params,
