479016bb0672f92c3cbb632655f68c64359faebf
Christian Duffau-Rasmussen <cd@northbridgedata.com>
2020-04-21 23:46:33 +0200

    Bug in data prep order.

--- scripts/run_data_prep.py
+++ scripts/run_data_prep.py
@@ -27,15 +27,6 @@ def main():
     csv_hosp_beds_pattern = op.join(config.WORLD_BANK_RAW_DATA_DIR, 'hospital_beds/API_SH.MED.BEDS.ZS*.csv')
     csv_pop_pattern = op.join(config.WORLD_BANK_RAW_DATA_DIR, 'population/API_SP.POP.TOTL*.csv')
 
-    logger.info('Running data prep of CSSEGI data ...')
-    run_cssegi_prep(
-        csv_file_paths=[confirmed_csv_file_path, deaths_csv_file_path, recovered_csv_file_path],
-        known_errors=None,
-        world_pop_file=world_pop_df,
-        world_hosp_beds_file=world_hosp_beds_df,
-        output_file=cssegi_preproc_file_path
-    )
-
     logger.info('Running data prep of World Bank population data ...')
     csv_filenames = glob(csv_pop_pattern)
     assert len(csv_filenames) == 1, f'len(csv_filenames) = {len(csv_filenames)} != 1'
@@ -45,6 +36,15 @@ def main():
     csv_filenames = glob(csv_hosp_beds_pattern)
     assert len(csv_filenames) == 1, f'len(csv_filenames) = {len(csv_filenames)} != 1'
     run_hosp_beds_prep(csv_filenames[0], output_folder=world_bank_preproc_data_dir)
+
+    logger.info('Running data prep of CSSEGI data ...')
+    run_cssegi_prep(
+        csv_file_paths=[confirmed_csv_file_path, deaths_csv_file_path, recovered_csv_file_path],
+        known_errors=None,
+        world_pop_file=world_pop_df,
+        world_hosp_beds_file=world_hosp_beds_df,
+        output_file=cssegi_preproc_file_path
+    )
 
 
 if __name__ == '__main__':
